ea16b1f96258862db118dba6968a51e3b160b4e8
Walid Cherhane <wcherhane17@gmail.com>
2022-09-04 00:00:22 +0100

    load todos :D

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,29 @@
 import * as React from "react";
 import { Button, Grid } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { todosAtom } from "../store";
+import { useAtom } from "jotai";
 
 /*
 JSON source: https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json
 */
 
 function TopBar() {
+  const [todos, setTodos] = useAtom(todosAtom);
+  const fetchTodos = async () => {
+    return fetch(
+      "https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos([...todos, ...data]);
+      });
+  };
+
   return (
     <Grid pt={2} templateColumns="1fr 1fr" columnGap="3">
       <ColorModeSwitcher />
-      <Button>Load</Button>
+      <Button onClick={() => fetchTodos()}>Load</Button>
     </Grid>
   );
 }
